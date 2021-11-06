6887a21c2b52927b74d52eab4e010745895f79da
Ranuga-Disansa <79456372+Programmer-RD-AI@users.noreply.github.com>
2021-11-06 22:08:45 +0530

    Add files via upload

---
+++ src/Navbar.js
@@ -0,0 +1,13 @@
+const NavBar = () => {
+  return (
+    <nav className="nav">
+      <h1>The Dojo Blog</h1>
+      <div className="links">
+          <a href="/">Home</a>
+          <a href="/create">New Blog</a>
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
