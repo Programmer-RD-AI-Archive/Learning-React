622e641fe7cd07ce32b132ca61c807727aefd302
Ranuga-Disansa <79456372+Programmer-RD-AI@users.noreply.github.com>
2021-11-09 11:32:44 +0530

    Add files via upload

--- src/Home.js
+++ src/Home.js
@@ -1,31 +1,18 @@
-import { useEffect, useState } from "react";
 import BlogList from "./BlogList";
+import useFetch from "./useFetch";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState(null);
-
-  const [name, setName] = useState("mario");
-
-  const handleDelete = (id) => {
-    const newBlogs = blogs.filter((blog) => blog.id !== id);
-    setBlogs(newBlogs);
-  };
-
-  useEffect(() => {
-    fetch("http://localhost:8000/blogs")
-      .then((res) => {
-        return res.json();
-      })
-      .then((data) => {
-        setBlogs(data);
-      });
-  }, []);
+  const {
+    data: blogs,
+    isPending,
+    error,
+  } = useFetch("http://localhost:8000/blogs");
 
   return (
     <div className="home">
-      {blogs && (
-        <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
-      )}
+      {error && <div>{error}</div>}
+      {blogs && <BlogList blogs={blogs} title="All Blogs" />}
+      {isPending && <div>Loading...</div>}
     </div>
   );
 };
